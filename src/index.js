39cd770505cf49dc327da86d8284373971e340bf
Dasharath Chaudhary <dasharathchaudhary61399@gmail.com>
2026-03-25 20:23:42 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+  app.on("error", (error) => {
+    console.log("ERROR - :", error);
+    throw error
+  })
+  
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port : ${process.env.PORT}`);
+  })
+
+}).catch((err) => {
+  console.log("Mongo DB connection failed !!! ", err)
+})
 
 
 

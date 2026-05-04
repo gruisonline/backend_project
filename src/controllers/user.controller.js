93f1beae8d4a78598af9691f6d3b3fb561538fcc
Dasharath Chaudhary <dasharathchaudhary61399@gmail.com>
2026-05-04 17:32:49 +0530

    Add comment, like, playlist, and tweet models with Mongoose schemas

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -438,6 +438,8 @@ const getWatchHistory = asyncHandler(async (req, res) => {
     }
   ])
 
+  console.log(user)
+
   return res.status(200).json(
     new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully")
   )
